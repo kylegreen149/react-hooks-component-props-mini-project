25311f37dc912d94a8490c248671280527a11f1c
Kyle Greenidge <kylegreen149@gmail.com>
2024-08-12 17:00:46 -0400

    Done

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,7 @@ import Article from "./Article"
 function ArticleList() {
     const allPosts = blogData.posts.map(post => {
         return (
-            <Article key={post.id} title={post.title} data={post.date} preview={post.preview}/>
+            <Article key={post.id} title={post.title} date={post.date} preview={post.preview}/>
         )
     })
     console.log(allPosts)
